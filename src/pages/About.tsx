d4e2623365e7683995ed5b01a028aac967a55543
Diki Iskandar <diki.iskandar@mandirisekuritas.co.id>
2025-07-07 15:42:54 +0700

    create react router

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the About Page</h1>
+            <p>This page is built with React and TypeScript.</p>
+        </div>
+    );
+};
+
+export default About;
